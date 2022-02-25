c73e08e34022ca05be47e471900b9fdef2b00e78
Vitor Azevedo Padovani <98499539+Vitor-Padovani@users.noreply.github.com>
2022-02-25 16:50:36 -0300

    basic menu

--- main.py
+++ main.py
@@ -1,13 +1,58 @@
 import curses
 
-def main(scr): # scr = screen
+def title(scr, y=0, txt=''):
+    title_width = 60
+
+    scr.addstr(y, 0, '-'*(title_width+3) )
+    scr.addstr(y+1, 0, f'{( ( title_width-len(txt) ) // 2 )*"="}< {txt} >{( ( title_width-len(txt) ) // 2 )*"="}')
+    scr.addstr(y+2, 0, '-'*(title_width+3) )
+
+def start_screen(scr):
+
+    while True:
+        scr.clear()
+
+        title(scr, txt='Jokenpô')
+
+        scr.addstr('''
+        J - jogar | I - instruções | ESC - sair
+        ''')
+
+        scr.refresh()
+
+        key = scr.getkey()
+        if ord(key) == 27:
+            break
+        elif ord(key) == 106:
+            pass
+            # jogar
+        elif ord(key) == 105:
+            instructions_screen(scr)
+
+def instructions_screen(scr):
+    scr.clear()
+
+    title(scr, txt='Instruções')
     
-    scr.clear() # cleans all screen content
+    scr.addstr('''
+    ESC - sair
+    ''')
+    scr.addstr('''
+    O jogo consiste em escolher uma das 3 opções de ataque,
+    tanto o jogador, quanto o computador. Após isso,
+    o resultado é mostrado, e as pontuações registradas.
+    Mas claro que o melhor jeito de aprender é jogando!
+    ''')
 
-    scr.addstr('Olá, Mundo!')
+    scr.refresh()
 
-    scr.refresh() # relaods the screen content
+    while True:
+        key = scr.getkey()
+        if ord(key) == 27:
+            break
 
-    scr.getkey() # waits for the user to type
+def main(scr): # scr = screen
+
+    start_screen(scr)
 
 curses.wrapper(main) # Allow Curses to control the terminal window
